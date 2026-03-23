ea3d809a303f76354c4ccb5fdd77c80e45727bcc
_@Nazib_07 <nazib9007.ice.bup@gmail.com>
2026-03-23 23:53:34 +0600

    Update index.js
    Reorganize event listener setup and remove comments.

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+let parent = document.querySelector('#parent');
+let form = document.querySelector('#form');
+let button = document.querySelector('#btn');
+
+
+/// at first remove the comment...
+// button.addEventListener('click', listen);
+// form.addEventListener('click', listen);
+// parent.addEventListener('click', listen);
+
+//Js Bubbling...
+//Here atfirst come child element then parent element..
+// function listen (enent) {
+//     console.log(event.currentTarget);
+//}
+
+// function listen (event){
+//     console.log(this.tagName);
+// }
+
+//Js Capturing...
+//It just reverse the tag name. parent parent come first then child...
+
+// button.addEventListener('click', listen, {
+//     capture: true,
+// });
+// form.addEventListener('click', listen, {
+//     capture: true,
+// });
+// parent.addEventListener('click', listen, {
+//     capture: true,
+// });
+
+// function listen (){
+//     console.log(this.tagName);
+// }
+
+
 let button = document.querySelector('#btn');
 button.addEventListener('click', (e)=>{
     let key = [];
